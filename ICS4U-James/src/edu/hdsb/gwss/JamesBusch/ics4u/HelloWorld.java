b3991eb103796bdba3c0a1f9add5c14222e1b3d1
jamers444 <jamers444@10.96.176.154>
2018-02-06 08:50:49 -0500

    Added intro comments

--- ICS4U-James/src/edu/hdsb/gwss/JamesBusch/ics4u/HelloWorld.java
+++ ICS4U-James/src/edu/hdsb/gwss/JamesBusch/ics4u/HelloWorld.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *James Busch
+ *Tues, Feb, 2018
+ *Print "Hello world" to consol
  */
 package edu.hdsb.gwss.JamesBusch.ics4u;
 
