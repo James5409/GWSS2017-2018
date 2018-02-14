badae345b63e50a171d3a22a6c9307fb6d7709d4
jamers444 <jamers444@10.96.176.154>
2018-02-14 11:56:16 -0500

    finished 3 of 4 parts of moview review assignment added java doc info and basic documantaion added new method to get the total times a word recurse and the total score from each word

--- ICS4U-James/src/edu/hdsb/gwss/JamesBusch/ics4u/u1/MovieReview.java
+++ ICS4U-James/src/edu/hdsb/gwss/JamesBusch/ics4u/u1/MovieReview.java
@@ -5,6 +5,7 @@ package edu.hdsb.gwss.JamesBusch.ics4u.u1;
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * Movie Review Assignment
@@ -13,12 +14,24 @@ import java.util.Scanner;
  * @version 2017-18.S2
  */
 public class MovieReview {
-
+    
+    /**
+     *
+     */
+    public static final int WORD_COUNT = 0;
+    public static final int TOTAL_SCORE = 1;
+    //to give getting array numbers meaning
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
     public static void main( String[] args ) throws Exception {
 
         // MOVIE REVIEW FILE
         File reviews = new File( "/Users/jamers444/NetBeansProjects/ICS4U-James/ICS4U-James/data/movie.review/MovieReviews.txt" );
-
+        File wordList = new File("/Users/jamers444/NetBeansProjects/ICS4U-James/ICS4U-James/data/movie.review/negTest.txt");
+        //file inilization
     }
 
     /**
@@ -27,17 +40,12 @@ public class MovieReview {
      * @param word the key word the review must contain.
      * @param reviews the file that contains the movie reviews.
      * @return the number of reviews that contain the key work at least once.
+     * @throws java.lang.Exception
      */
     public static int wordCount( String word, File reviews ) throws Exception {
-        Scanner textFile = new Scanner(reviews);
-        int i = 0;
-        while(textFile.hasNextLine()){
-            i++;
-            textFile.nextLine();
-        }
-        
-       return i;
-       
+       int[] finalAnswers;
+       finalAnswers = readLineByLine(word, reviews);
+       return finalAnswers[WORD_COUNT]; 
     }
 
     /**
@@ -48,10 +56,12 @@ public class MovieReview {
      * @param reviews the file that contains the movie reviews.
      * @return the sum of the scores for reviews that contain the key work at
      * least once.
+     * @throws java.lang.Exception
      */
     public static int wordTotalScore( String word, File reviews ) throws Exception {
-
-        return -1;
+        int[] finalAnswers;
+        finalAnswers = readLineByLine(word, reviews);
+        return finalAnswers[TOTAL_SCORE];
     }
 
     /**
@@ -60,21 +70,100 @@ public class MovieReview {
      * @param word the key word the review must contain.
      * @param reviews reviews the file that contains the movie reviews.
      * @return the average score for the key word. Word Total Score / Word Count
+     * @throws java.lang.Exception
      */
     public static double wordAverage( String word, File reviews ) throws Exception {
-
-       return -1.0;
+        double totalScore;
+        double numOfWords;
+        int[] finalAnswers;
+        finalAnswers = readLineByLine(word, reviews);
+        totalScore = finalAnswers[TOTAL_SCORE];
+        numOfWords = finalAnswers[WORD_COUNT];
+        return totalScore/numOfWords;
        
     }
 
     /**
      * This method returns the average movie review score of the words in the
      * file, given th specified movie review file.
+     * @param wordList list of words to be tested
+     * @param reviews file of all reviews
+     * @return the average score of all words combined 
+     * @throws java.lang.Exception 
      */
     public static double sentenceAverage( File wordList, File reviews ) throws Exception {
+        Scanner userData = new Scanner(wordList);    
+        double currScore = 0;
+        int i = 0;
 
-        return -1.0;
-
+        while(userData.hasNextLine()){
+            currScore = currScore + wordAverage(userData.nextLine(),reviews);
+            i++;
+        }
+        return currScore / i;
     }
+    
+    /**
+     *
+     * @param wordList list of user input words
+     * @param reviews list of all reviews
+     * @throws Exception
+     */
+    public static void mutipleWordScore( File wordList, File reviews ) throws Exception{
+        Scanner userData = new Scanner(wordList);
+        
+        double currScore;
+        double bestScore = 0;
+        double worstScore = 4;
+        String bestWord = null;
+        String worstWord = null;
+        
+        while(userData.hasNextLine()){
+            currScore = wordAverage(userData.nextLine(),reviews);
+            if(currScore > bestScore){
+                bestScore = currScore;
+                bestWord = userData.toString();
+            }else if(currScore < worstScore){
+                worstScore = currScore;
+                worstWord = userData.toString();
+            }   
+            System.out.println("The worst word is " + worstWord + " with a " + worstScore);
+            System.out.println("The best word is " + bestWord + "with a " + bestScore);   
+        }
+    }
+    
 
+    
+    
+    /**
+     *
+     * @param word the key word that wants to be found
+     * @param reviews the list of reviews
+     * @return an array with wordCount at i = 0 and totalScore at i = 1
+     * @throws java.lang.Exception
+     */
+    public static int[] readLineByLine(String word, File reviews) throws Exception {//a = 0 is word count, a = 1 is score count
+        Scanner textFile = new Scanner(reviews);
+        word = word.toLowerCase();
+        int[] arrayTotal = new int[2];
+        int currScore;
+        int i = 0;
+        int totalScore = 0;
+        boolean hasWord;
+        while(textFile.hasNextLine()){
+            hasWord = false;
+            StringTokenizer st = new StringTokenizer(textFile.nextLine().toLowerCase());
+            currScore = Integer.parseInt(st.nextToken());
+            while(st.hasMoreTokens()){
+                if(st.nextToken().equals(word) && hasWord == false){
+                    hasWord = true;
+                    totalScore = currScore + totalScore;
+                    i++;
+                }      
+            }
+        }
+        arrayTotal[0] = i;
+        arrayTotal[1] = totalScore;
+        return arrayTotal;
+    }
 }
