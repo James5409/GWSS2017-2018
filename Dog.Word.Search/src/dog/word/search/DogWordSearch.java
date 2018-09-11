82340ce04f5ea459ec8d06c25ab703fb51005e2d
jamers444 <jamers444@10.96.176.154>
2018-09-11 16:56:30 -0400

    Finished summative

---
+++ Dog.Word.Search/src/dog/word/search/DogWordSearch.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dog.word.search;
+
+/**
+ *
+ * @author jamers444
+ */
+public class DogWordSearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    static char[][] wordSearch = {
+        {'D', 'G', 'O', 'O', 'D', 'D', 'O', 'D', 'G', 'O', 'O', 'D', 'D', 'O'},
+        {'O', 'D', 'O', 'O', 'G', 'G', 'G', 'D', 'O', 'D', 'G', 'O', 'G', 'G'},
+        {'O', 'G', 'O', 'G', 'D', 'O', 'O', 'D', 'G', 'O', 'O', 'D', 'D', 'D'},
+        {'D', 'G', 'D', 'O', 'O', 'O', 'G', 'G', 'O', 'O', 'G', 'D', 'G', 'O'},
+        {'O', 'G', 'D', 'G', 'O', 'G', 'D', 'G', 'O', 'G', 'G', 'O', 'G', 'D'},
+        {'D', 'D', 'D', 'G', 'D', 'D', 'O', 'D', 'O', 'O', 'G', 'D', 'O', 'O'},
+        {'O', 'D', 'G', 'O', 'G', 'G', 'D', 'O', 'O', 'G', 'G', 'O', 'O', 'D'}
+
+    };
+    
+ 
+    
+
+    public static void main(String[] args) {
+        boolean wordFound = false;
+        
+        for (int c = 0; c < wordSearch.length; c++) {
+            for (int r = 0; r < wordSearch[0].length; r++) {
+                
+            }
+            
+        }
+    }
+    
+    public boolean checkUp(int c, int r){
+       char letterOne;
+       char
+        
+    }
+
+}
