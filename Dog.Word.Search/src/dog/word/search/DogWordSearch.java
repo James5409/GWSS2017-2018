cd327944e179fddc11a41b1422fefa09f71cc846
Owner <Owner@10.10.21.49>
2018-09-11 20:49:35 -0400

    Finshed basic search(up down left and right)

--- Dog.Word.Search/src/dog/word/search/DogWordSearch.java
+++ Dog.Word.Search/src/dog/word/search/DogWordSearch.java
@@ -19,30 +19,99 @@ public class DogWordSearch {
         {'O', 'D', 'O', 'O', 'G', 'G', 'G', 'D', 'O', 'D', 'G', 'O', 'G', 'G'},
         {'O', 'G', 'O', 'G', 'D', 'O', 'O', 'D', 'G', 'O', 'O', 'D', 'D', 'D'},
         {'D', 'G', 'D', 'O', 'O', 'O', 'G', 'G', 'O', 'O', 'G', 'D', 'G', 'O'},
-        {'O', 'G', 'D', 'G', 'O', 'G', 'D', 'G', 'O', 'G', 'G', 'O', 'G', 'D'},
+        {'O', 'G', 'G', 'O', 'D', 'G', 'D', 'G', 'O', 'G', 'G', 'O', 'G', 'D'},
         {'D', 'D', 'D', 'G', 'D', 'D', 'O', 'D', 'O', 'O', 'G', 'D', 'O', 'O'},
         {'O', 'D', 'G', 'O', 'G', 'G', 'D', 'O', 'O', 'G', 'G', 'O', 'O', 'D'}
 
     };
-    
- 
-    
 
     public static void main(String[] args) {
-        boolean wordFound = false;
+        boolean wordFound = false; 
         
-        for (int c = 0; c < wordSearch.length; c++) {
-            for (int r = 0; r < wordSearch[0].length; r++) {
+        for (int r = 0; r < wordSearch.length && wordFound == false; r++) {
+            for (int c = 0; c < wordSearch[0].length && wordFound == false; c++) {
+                //System.out.println("row:" + r + " col:" + c );
                 
+                if(checkUp(c, r) == true){
+                    System.out.println("the word was found at row:" + (r + 1) + " and col:" + (c + 1) + " going up");
+                    wordFound = true;
+                }
+                if(checkDown(c, r) == true){
+                    System.out.println("the word was found at row:" + (r + 1) + " and col:" + (c + 1) + " going Down");
+                    wordFound = true;
+                }
+                if(checkLeft(c, r) == true){
+                    System.out.println("the word was found at row:" + (r + 1) + " and col:" + (c + 1) + " going Left");
+                    wordFound = true;
+                }
+                if(checkRight(c, r) == true){
+                    System.out.println("the word was found at row:" + (r + 1) + " and col:" + (c + 1) + " going Right");
+                    wordFound = true;
+                }
+                
+                
+//                checkUp(c, r);
+//                checkDown(c, r);
+//                checkLeft(c, r);
+//                checkRight(c, r);
             }
-            
+
         }
     }
-    
-    public boolean checkUp(int c, int r){
-       char letterOne;
-       char
-        
+
+    public static boolean checkUp(int c, int r) {
+        String foundWord = null;
+        boolean wordFound = false;
+        boolean valid = false;
+        if (r >= 2 && r <= 5) {
+            valid = true;
+            foundWord = "" + wordSearch[r][c] + wordSearch[r - 1][c] + wordSearch[r - 2][c];
+        }
+        if (valid == true) {
+            wordFound = foundWord.equals("DOG");
+        }
+        return wordFound;
     }
 
+    public static boolean checkDown(int c, int r) {
+        String foundWord = null;
+        boolean wordFound = false;
+        boolean valid = false;
+        if (r >= 0 && r <= 3) {
+            valid = true;
+            foundWord = "" + wordSearch[r][c] + wordSearch[r + 1][c] + wordSearch[r + 2][c];
+        }
+        if (valid == true) {
+            wordFound = foundWord.equals("DOG");
+        }
+        return wordFound;
+    }
+
+    public static boolean checkRight(int c, int r) {
+        String foundWord = null;
+        boolean wordFound = false;
+        boolean valid = false;
+        if (c >= 0 && c <= 11) {
+            valid = true;
+            foundWord = "" + wordSearch[r][c] + wordSearch[r][c + 1] + wordSearch[r][c + 2];
+        }
+        if (valid == true) {
+            wordFound = foundWord.equals("DOG");
+        }
+        return wordFound;
+    }
+
+    public static boolean checkLeft(int c, int r) {
+        String foundWord = null;
+        boolean wordFound = false;
+        boolean valid = false;
+        if (c >= 2 && c <= 14) {
+            valid = true;
+            foundWord = "" + wordSearch[r][c] + wordSearch[r][c - 1] + wordSearch[r][c - 2];
+        }
+        if (valid == true) {
+            wordFound = foundWord.equals("DOG");
+        }
+        return wordFound;
+    }
 }
